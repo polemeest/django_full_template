87a824cb597dca18aa5e0a24ea425f410c6adcce
polemeest <doran6112@gmail.com>
2023-12-26 17:09:21 +0400

    added apps.router and docker package

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,5 +1,6 @@
 import os
 import environ
+from datetime import timedelta
 
 # Implicit env variables
 env = environ.Env(
@@ -60,6 +61,7 @@ INSTALLED_APPS = [
     # third party
     'rest_framework',
     'drf_spectacular',
+    'rest_framework_simplejwt',
 
 ]
 
@@ -92,9 +94,7 @@ USE_TZ = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,7 +114,7 @@ ASGI_APPLICATION = 'config.asgi.application'
 REDIS_HOST = env('REDIS_HOST')
 REDIS_PORT = env('REDIS_PORT')
 
-CELERY_TIMEZONE = TIME_ZONE
+CELERY_TIMEZONE = TIME_ZONE 
 CELERY_BROKER_URL = env('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND')
 CELERY_ACCEPT_CONTENT = [env('CELERY_ACCEPT_CONTENT')]
@@ -214,3 +214,46 @@ LOGIN_URL = env('LOGIN_URL')
 LOGIN_REDIRECT_URL = env('LOGIN_REDIRECT_URL')
 LOGOUT_REDIRECT_URL = env('LOGOUT_REDIRECT_URL')
 LOGOUT_URL = env('LOGOUT_URL')
+
+
+# SimpleJWT
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView, )
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView, )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    # auth
+    path('api/v1/api-auth/', include('rest_framework.urls'), name='drf_auth'),
+    path('api/v1/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # swagger
+    path('api/v1/api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # dev
+    path('api/v1/', include('apps.router'), name='main_router'),
 ]
